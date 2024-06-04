6ac734b09303e6876b73e2b39b5973c2649dfb40
Kuroyukimaru <oranggabut712@gmail.com>
2024-06-04 11:05:55 +0700

    membuat try dan cathc di dalam fungsi main

--- ExeptionStandard/ExeptionStandard.cpp
+++ ExeptionStandard/ExeptionStandard.cpp
@@ -5,9 +5,18 @@
 //untuk obyek array yang akan kita gunakan
 using namespace std;
 
+int main() {
 
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = { 8, 4, 2 };
+        //pesan array integer 3 elemen
+        cout << data.at(5) << endl;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen */
+    }
 }
