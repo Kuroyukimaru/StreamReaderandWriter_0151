2b61428c845d7f784bc4fb26742fbe3bc45bc7b7
Kuroyukimaru <oranggabut712@gmail.com>
2024-06-04 10:57:24 +0700

    Membuat catch

--- ManualExeption/ManualExeption.cpp
+++ ManualExeption/ManualExeption.cpp
@@ -7,5 +7,12 @@ int main(){
 		//throw 0.5; //melemparkan sebuuah integer maka
 		cout << "Pertnyataan tidak akan dieksekusi" << endl;
 	}
-
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << " Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		cout << "Default Pengecualian dieksekusi" << endl;
+	}
+	return 0;
 }
