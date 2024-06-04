5c29e7137680ce30548ed06bd8d8a7b62c898124
Kuroyukimaru <oranggabut712@gmail.com>
2024-06-04 10:36:21 +0700

    menunjuk kesebuah nama file

---
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -0,0 +1,18 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+    string baris;
+    string NamaFile;
+    cout << "Masukan nama file : ";
+    cin >> NamaFile;
+
+    /// membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk kesebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+}
