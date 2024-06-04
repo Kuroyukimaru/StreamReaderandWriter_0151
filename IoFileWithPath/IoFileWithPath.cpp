e47a4385a500d63ed3f8887ff53b89d3ab19a0b3
Kuroyukimaru <oranggabut712@gmail.com>
2024-06-04 10:47:50 +0700

    membuat pengulangan jika ada file dan jika tidak ditemukan file

--- IoFileWithPath/IoFileWithPath.cpp
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -22,9 +22,30 @@ int main(){
         cout << "- ";
         // mendapatkan setiap karakter dalam satu baris
         if (baris == "q") break;
+        getline(cin, baris);
         //menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
 
+    //selesai dalam menulis sekarang tutup fileny
+    outfile.close();
 
+    //Membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika  ada file maka
+    if (infile.is_open()) {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
